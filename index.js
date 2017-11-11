4b8454305a10222c4c02a5430af9d5b07758021b
William Beltran <wbeltran13@gmail.com>
2017-11-11 15:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = ... ;
+
+shortGreeting;
